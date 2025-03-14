1c6d5ec945cd575e4e2cef9eadfc39356eac0118
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-03-14 12:47:37 +0100

    done

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cat cdlinux.ftp.log | grep "OK" | cut -d '"' -f 2,4 | sort | uniq | cut -d '"' -f 2 | grep -o "cdlinux-.*" | sed "s#?.*##" | sort > wynik.txt
-cat cdlinux.www.log | cut -d " " -f 1,7,9 | grep '200$' | sort | uniq | cut -d " " -f 2 | grep -o "cdlinux-.*" | sed "s#?.*##" | sort >> wynik.txt
-cat wynik.txt | sort | uniq -c | sort -rn
+echo "FTP:"
+cat cdlinux.ftp.log | grep "OK DOWNLOAD" | cut -d '"' -f 2,4 | sort | uniq | cut -d '"' -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#.*/##" | sort | uniq -c | sort -rn
+
+echo -e "\nWWW:"
+cat cdlinux.www.log | cut -d " " -f 1,7,9 | grep '200$' | sort | uniq | cut -d " " -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#.*/##" | sort | uniq -c | sort -rn
+
+echo -e "\nSumaryczne:"
+( 
+    cat cdlinux.ftp.log | grep "OK DOWNLOAD" | cut -d '"' -f 2,4 | sort | uniq | cut -d '"' -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#.*/##"
+    cat cdlinux.www.log | cut -d " " -f 1,7,9 | grep '200$' | sort | uniq | cut -d " " -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#.*/##"
+) | sort | uniq -c | sort -rn
