47b13131c68ecc83530a6413578e8fd75f573330
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-03-14 01:02:09 +0100

    Update skrypt11.sh

--- skrypt11.sh
+++ skrypt11.sh
@@ -7,4 +7,7 @@ echo -e "\nWyniki z WWW:"
 cat cdlinux.www.log | cut -d " " -f 1,7,9 | grep '200$' | sort | uniq | cut -d " " -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#?.*##" | sort | uniq -c | sort -rn
 
 echo -e "\nWyniki sumaryczne:"
-cat cdlinux.ftp.log cdlinux.www.log | grep "OK\| 200$" | cut -d '"' -f 2,4 | awk '{print $NF}' | grep -o "cdlinux-.*\.iso" | sed "s#?.*##" | sort | uniq -c | sort -rn
+( 
+    cat cdlinux.ftp.log | grep "OK" | cut -d '"' -f 2,4 | sort | uniq | cut -d '"' -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#?.*##"
+    cat cdlinux.www.log | cut -d " " -f 1,7,9 | grep '200$' | sort | uniq | cut -d " " -f 2 | grep -o "cdlinux-.*\.iso" | sed "s#?.*##"
+) | sort | uniq -c | sort -rn
