90df9beb67d2400bc3e5b800b208a31f5590e2dd
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-03-28 13:42:12 +0100

    Update skrypt44.sh

--- skrypt44.sh
+++ skrypt44.sh
@@ -11,11 +11,16 @@ done < mail.txt
 echo -e "\nCzęść 2"
 
 curl -s "https://raw.githubusercontent.com/bnokoro/Data-Science/refs/heads/master/countries%20of%20the%20world.csv" | \
-awk -F',' 'NR > 1 && $2 ~ /^[ ]*EUROPE[ ]*$/ {
-    gsub(/^[ \t]+|[ \t]+$/, "", $1);  # Country
-    gsub(/^[ \t]+|[ \t]+$/, "", $3);  # Population
-    gsub(/^[ \t]+|[ \t]+$/, "", $4);  # Area
-    printf "  { \"country\": \"%s\", \"population\": %d, \"area\": %d },\n", $1, $3, $4
+awk -F',' '{
+    # $1 = Country, $2 = Region, $3 = Population, $4 = Area
+    region = $2
+    gsub(/^[ \t]+|[ \t]+$/, "", region)  # usuń spacje
+
+    if (region ~ /EUROPE/) {
+        gsub(/^[ \t]+|[ \t]+$/, "", $1)  # Country
+        gsub(/^[ \t]+|[ \t]+$/, "", $3)  # Population
+        gsub(/^[ \t]+|[ \t]+$/, "", $4)  # Area
+        printf "  { \"country\": \"%s\", \"population\": %d, \"area\": %d },\n", $1, $3, $4
+    }
 }' | sed '$s/,$//' | \
 awk 'BEGIN { print "[" } { print } END { print "]" }'
-
