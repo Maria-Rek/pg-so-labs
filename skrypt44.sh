c7f28fe9cef788344025fb1715e2da50d0389a6b
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-03-28 13:38:53 +0100

    Update skrypt44.sh

--- skrypt44.sh
+++ skrypt44.sh
@@ -11,11 +11,11 @@ done < mail.txt
 echo -e "\nCzęść 2"
 
 curl -s "https://raw.githubusercontent.com/bnokoro/Data-Science/refs/heads/master/countries%20of%20the%20world.csv" | \
-grep '^"[^"]*","[^"]*","[^"]*","[^"]*","[^"]*"," *EUROPE *"' | \
-awk -F',' '{
+awk -F',' 'NR > 1 && $2 ~ /^[ ]*EUROPE[ ]*$/ {
     gsub(/^[ \t]+|[ \t]+$/, "", $1);  # Country
     gsub(/^[ \t]+|[ \t]+$/, "", $3);  # Population
     gsub(/^[ \t]+|[ \t]+$/, "", $4);  # Area
     printf "  { \"country\": \"%s\", \"population\": %d, \"area\": %d },\n", $1, $3, $4
 }' | sed '$s/,$//' | \
 awk 'BEGIN { print "[" } { print } END { print "]" }'
+
