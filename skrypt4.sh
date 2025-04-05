6ca2c8132cb160326bfaf692f3a2a7a201460550
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-04-05 16:40:01 +0200

    Update skrypt4.sh

--- skrypt4.sh
+++ skrypt4.sh
@@ -10,21 +10,11 @@ done < mail.txt
 
 echo -e "\nCzęść 2"
 
-curl -s "https://raw.githubusercontent.com/bnokoro/Data-Science/refs/heads/master/countries%20of%20the%20world.csv" | \
-awk -F',' 'NR > 1 {
-    region = $2
-    gsub(/^[ \t]+|[ \t]+$/, "", region)
-    if (region ~ /EUROPE/) {
-        country = $1
-        population = $3
-        area = $4
+filename='countries.csv'
 
-        gsub(/^[ \t]+|[ \t]+$/, "", country)
-        gsub(/^[ \t]+|[ \t]+$/, "", population)
-        gsub(/^[ \t]+|[ \t]+$/, "", area)
-
-        if (population ~ /^[0-9]+$/ && area ~ /^[0-9]+$/) {
-            printf "  { \"country\": \"%s\", \"population\": %s, \"area\": %s },\n", country, population, area
-        }
-    }
-}' | sed '$s/,$//' | awk 'BEGIN { print "[" } { print } END { print "]" }'
+echo "[" > wynik.json
+tail -n +2 "$filename" | while IFS=',' read -r rank cca3 country capital continent population2022 _; do
+    if [[ "$continent" == "Europe" ]]; then
+        echo " {\"country\":\"$country\",\"population\":\"$population2022\"},">> wynik.json
+    fi
+done
