b4ea4e5a4e2403cdb4bdef2d57e8bb38786f9001
marysia <41950773+Maria-Rek@users.noreply.github.com>
2025-04-11 10:03:32 +0200

    c

---
+++ skrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author           : Imie Nazwisko ( email )
+# Created On       : data
+# Last Modified By : Imie Nazwisko ( email )
+# Last Modified On : data 
+# Version          : wersja
+#
+# Description      :
+# Opis
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
+. skrypt.rc
